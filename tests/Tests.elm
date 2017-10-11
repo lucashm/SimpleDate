3089f0ac9213334bcc5d4ddd7e36ba117269def3
Lucas Malta <lucas50xd@hotmail.com>
2017-10-11 13:17:14 -0300

    configuring travis

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,10 +1,7 @@
 module Tests exposing (..)
+
 import Test exposing (..)
-import Expect
-import Tests.SimpleDate exposing (..)
-
-
--- Check out http://package.elm-lang.org/packages/elm-community/elm-test/latest to learn more about testing in Elm!
+import Tests.SimpleDate
 
 
 all : Test
